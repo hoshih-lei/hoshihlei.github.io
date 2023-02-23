a6846d8bb46c508b625a783d36ed39ac735e6dd6
hoshih-lei <105062139+hoshih-lei@users.noreply.github.com>
2023-02-23 17:16:57 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let myimg=document.querySelector('img');
+myimg.onclick=function() {
+	let mysrc=myimg.getAttribute('src');
+	if (mysrc === 'img/戈薇.jpg') {
+		myimg.setAttribute('src','img/七宝.jpg');
+	}
+	else {
+		myimg.setAttribute('src','img/戈薇.jpg');
+	}
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('您是？');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = '请收下这首诗，' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = '请收下这首诗，' + storedName;
+}
+myButton.onclick = function() {
+   setUserName();
+}
